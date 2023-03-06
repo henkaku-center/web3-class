df540998525b0eaeaa454bad23f44b0d7c8fe36e
Vincenzo Ferrara <vinceferro92@gmail.com>
2023-03-06 15:57:09 +0100

    Adds reference for Ticket.

--- src/reference/Ticket.sol
+++ src/reference/Ticket.sol
@@ -3,30 +3,47 @@
 pragma solidity ^0.8.9;
 
 import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/token/ERC721/ERC721.sol";
 
-contract Ticket {
-    IERC20 public immutable token;
+error NotEnoughTokens();
+error TransferFailed();
+error InvalidOwner();
+
+contract Ticket is ERC721 {
     uint256 public immutable mintPrice;
-    uint256 nextTicketId = 0;
-    mapping(uint256 => address) public ownerOf;
+    IERC20 public immutable token;
+    uint256 public nextTokenId = 0;
 
-    event TicketMinted(uint256 indexed ticketId, address indexed owner);
-
-    constructor(IERC20 _token, uint256 _mintPrice) {
+    constructor(IERC20 _token, uint256 _mintPrice, string memory _name, string memory _symbol) ERC721(_name, _symbol) {
         token = _token;
         mintPrice = _mintPrice;
     }
 
-    function mint(address _to) public {
-        token.transferFrom(msg.sender, address(this), mintPrice);
-        ownerOf[nextTicketId] = _to;
-        ++nextTicketId;
+    /// @notice This function is used to mint a new ticket.
+    /// @param to Address of the new owner.
+    /// @dev The caller must have enough tokens to pay for the ticket.
+    function mint(address to) public {
+        if (token.balanceOf(msg.sender) < mintPrice) {
+            revert NotEnoughTokens();
+        }
+        if (!token.transferFrom(msg.sender, address(this), mintPrice)) {
+            revert TransferFailed();
+        }
 
-        emit TicketMinted(nextTicketId, _to);
+        uint256 tokenId = nextTokenId;
+        ++nextTokenId;
+
+        _safeMint(to, tokenId);
     }
 
-    function burn(uint256 _ticketId) public {
-        require(ownerOf[_ticketId] == msg.sender, "Only the owner can burn the ticket");
-        delete ownerOf[_ticketId];
+    /// @notice This function is used to burn a ticket.
+    /// @param tokenId Id of the ticket to burn.
+    /// @dev The caller must be the owner or approved address of the ticket.
+    function burn(uint256 tokenId) public {
+        if (!_isApprovedOrOwner(msg.sender, tokenId)) {
+            revert InvalidOwner();
+        }
+
+        _burn(tokenId);
     }
 }
